3c35aba42dfe55d762f0d902ccb9b10f55759152
Marcoski05 <marco.crivas24@bishopireton.org>
2023-06-06 20:00:29 -0400

    TTT_Test file and tests every four seconds

--- TicTacToeHashFuction/src/TicTacToeHashCode.java
+++ TicTacToeHashFuction/src/TicTacToeHashCode.java
@@ -8,16 +8,22 @@ public class TicTacToeHashCode extends Board {
 
 	boolean[] winners; // True if the hash string that maps to this index is a winner, false otherwise
 
-	TicTacToeHashCode(String s) throws FileNotFoundException {
+	TicTacToeHashCode(String s) {
 		super(s);
 		// TODO Instantiate/fill winners array.
-		winners = new boolean[(int) Math.pow(9, 3)];
+		winners = new boolean[(int) Math.pow(3, 9)];
 		File file = new File("TicTacToeWinners.txt");
-		Scanner sc = new Scanner(file);
+		Scanner sc;
+		try { 
+			sc = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("File \"TicTacToeWinners.txt\" not in working directory");
+			return;
+		}
 		while (sc.hasNextLine()) {
-			Board testBoard = new TicTacToeHashCode("temp");
-			testBoard.setBoardString(sc.nextLine());
-			winners[testBoard.myHashCode()] = true;
+			winners[hashCode(sc.nextLine())] = true;
 		}
 		sc.close();
 	}
@@ -33,16 +39,35 @@ public class TicTacToeHashCode extends Board {
 		for (int r = 0; r < 3; r++) {
 			for (int c = 0; c < 3; c++) {
 				switch (charAt(r, c)) {
-					case 'o':
-						code += Math.pow(3, placeValue);
-						break;
-					case 'x':
-						code += 2 * Math.pow(3, placeValue);
-						break;
+				case 'x':
+					code += Math.pow(3, placeValue);
+					break;
+				case 'o':
+					code += 2 * Math.pow(3, placeValue);
+					break;
 				}
 				placeValue++;
 			}
 		}
+		return code;
+	}
+
+	public int hashCode(String s) {
+		int placeValue = 0;
+		int code = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			switch (s.charAt(i)) {
+			case 'x':
+				code += Math.pow(3, placeValue);
+				break;
+			case 'o':
+				code += 2 * Math.pow(3, placeValue);
+				break;
+			}
+			placeValue++;
+		}
+
 		return code;
 	}
 
@@ -51,28 +76,38 @@ public class TicTacToeHashCode extends Board {
 	}
 
 	public boolean isWin(String s) {
-		Board testBoard = new TicTacToeHashCode("temp");
-		testBoard.setBoardString(s);
-		return winners[testBoard.myHashCode()];
+		return winners[hashCode(s)];
 	}
 
 	public static void main(String[] args) throws InterruptedException {
 		TicTacToeHashCode board = new TicTacToeHashCode("Tic Tac Toe");
 		String s = "";
-		java.util.Scanner kb = new java.util.Scanner(System.in);
+
+		File file = new File("TTT_Tests.txt");
+		Scanner sc;
+		try {
+			sc = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("File \"TTT_Tests.txt\" not in working directory");
+			return;
+		}
 
 		while (true) {
-			s = kb.nextLine();
+			s = sc.nextLine();
 			// String currentBoard = board.boardValues[(int)(Math.random()*
 			// board.boardValues.length)];
 			// board.show(currentBoard);
 			// board.setHashCode(board.myHashCode());
-			// TODO Update this line to call your isWin method.
-			// board.setWinner(TicTacToe.isWin(currentBoard));
-			TicTacToe ttt = new TicTacToe();
-			System.out.println(ttt.isWin(s));
 			board.show(s);
-			// Thread.sleep(4000);
+			board.setHashCodeLabel(board.myHashCode());
+			// TODO Update this line to call your isWin method.
+			board.setWinner(board.isWin(s));
+			// TicTacToe ttt = new TicTacToe();
+			// System.out.println(ttt.isWin(s));
+			board.show(s);
+			Thread.sleep(4000);
 			// board.displayRandomString();
 		}
 	}
